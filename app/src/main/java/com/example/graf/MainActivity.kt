fb1bffe0651b42db24026be9e990b83f14d8c3f5
Dominos0321 <dsch792@gmail.com>
2022-10-26 14:09:42 +0200

    poprawa layoutu i poczatek funkcjonalnosci

--- app/src/main/java/com/example/graf/MainActivity.kt
+++ app/src/main/java/com/example/graf/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.graf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var show_path = findViewById<Button>(R.id.show_path)
+        var vertex = findViewById<EditText>(R.id.vertex)
     }
 }
